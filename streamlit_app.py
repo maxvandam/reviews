054c25ef12bd815c2fd3a60374c6485583425d49
Max van Dam <45095472+maxvandam@users.noreply.github.com>
2021-10-18 11:46:47 +0200

    Create streamlit_app.py

---
+++ streamlit_app.py
@@ -0,0 +1,12 @@
+#import packages
+import pandas as pd
+import numpy as np
+import plotly.express as px
+import matplotlib.pyplot as plt
+import fuzzywuzzy
+import seaborn as sns
+import plotly.graph_objects as go
+import plotly.figure_factory as ff
+from statsmodels.formula.api import ols
+import gzip
+
